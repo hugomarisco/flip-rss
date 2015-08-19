f16edaece18cb69c834604f9c861697686c84a15
Hugo Marisco <0x6875676f@gmail.com>
2015-08-19 16:35:59 +0100

    Building a PHP backend service to replace Google Feed API

---
+++ rss_server.php
@@ -0,0 +1,17 @@
+<?php
+
+// Live @ https://flip-rss-hugomarisco-1.c9.io/rss.php?rss_url=<rssUrl>
+
+$client_ip = $_SERVER['REMOTE_ADDR'];
+$rss_url = $_GET['rss_url'];
+
+$url = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q={$rss_url}&userip={$client_ip}";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_REFERER, "http://github.com");
+$body = curl_exec($ch);
+curl_close($ch);
+
+print($body);
